ccc823103129f24b0e946972a6533f247a276a71
openlab <syntaxc4@gmail.com>
2012-01-11 03:58:57 -0500

    inital check in. (SDK 1.6, Config Transform)

--- Shared/Ogdi.Azure/Configuration/OgdiConfiguration.cs
+++ Shared/Ogdi.Azure/Configuration/OgdiConfiguration.cs
@@ -1,22 +1,21 @@
-﻿using Microsoft.WindowsAzure.ServiceRuntime;
+﻿using System.Configuration;
+using Microsoft.WindowsAzure.ServiceRuntime;
 
 namespace Ogdi.Azure.Configuration
 {
-	public sealed class OgdiConfiguration
-	{
-		/// <summary>
-		/// This method finds the value for the passed key from the 
-		/// AppSettings in XML or Windows Azure role environment.
-		/// </summary>
-		/// <param name="key">The value name.</param>
-		/// <returns>The value.</returns>
-		public static string GetValue(string key)
-		{
-			return
-				(RoleEnvironment.IsAvailable
-			       	? RoleEnvironment.GetConfigurationSettingValue(key)
-					:  System.Configuration.ConfigurationManager.AppSettings[key])
-				?? string.Empty;
-		}
-	}
+    public sealed class OgdiConfiguration
+    {
+        /// <summary>
+        /// This method finds the value for the passed key from the 
+        /// AppSettings in XML or Windows Azure role environment.
+        /// </summary>
+        /// <param name="key">The value name.</param>
+        /// <returns>The value.</returns>
+        public static string GetValue(string key)
+        {
+            return (RoleEnvironment.IsAvailable)
+                ? RoleEnvironment.GetConfigurationSettingValue(key)
+                : ConfigurationManager.AppSettings[key];
+        }
+    }
 }
